8930742d511d2cfe341aa3054daa6b9c02e59d2c
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-01 16:23:44 -0600

    added p tag to index.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirects_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body)
 end
