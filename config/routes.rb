a6439dc3f64c6a490e844b3ebf7e1df9a328b23f
Rosemary Thomas <rsm.thomas@gmail.com>
2015-06-01 16:25:16 -0600

    added static_pages controller with home and about page in views/static_pages/

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
